5cdf4e932cf7d1eb4f5aa73441930952492da113
Polarzz <57324714+Polarzz@users.noreply.github.com>
2020-08-13 15:06:27 +0100

    Create unix.py

---
+++ unix.py
@@ -0,0 +1,11 @@
+def _input():
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
