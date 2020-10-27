11a3edd0d6f31487e887b035fcdd2317d7b90ab2
Polarzz <57324714+Polarzz@users.noreply.github.com>
2020-10-27 21:50:42 +0000

    Update __init__.py

--- plugins/time/print/__init__.py
+++ plugins/time/print/__init__.py
@@ -1,5 +1,5 @@
 import time
 ARGUMENTS = 1
 def main(arg, cells, position):
-    print(time.ctime(int(arg[0])))
+    print(time.ctime(int(arg[0]), end=""))
     return {"cells": cells, "position": position}
